456b763212d2bed3597e14cebf497e8c903fc077
franktranvantu <59758589+franktranvantu@users.noreply.github.com>
2021-04-12 11:03:21 +0700

    Update

---
+++ src/main/java/com/franktran/springbootsecurity/controller/StudentController.java
@@ -0,0 +1,35 @@
+package com.franktran.springbootsecurity.controller;
+
+import com.franktran.springbootsecurity.model.Student;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+@RequestMapping("/students")
+public class StudentController {
+
+    private static final List<Student> STUDENTS = Arrays.asList(
+            new Student(1, "Frank", "frank@gmail.com"),
+            new Student(2, "Henry", "henry@gmail.com"),
+            new Student(3, "Bea", "bean@gmail.com")
+    );
+
+    @GetMapping
+    public List<Student> getAllStudent() {
+        return STUDENTS;
+    }
+
+    @GetMapping("/{id}")
+    public Student getStudentById(@PathVariable int id) {
+        return STUDENTS.stream()
+                .filter(student -> student.getId() == id)
+                .parallel()
+                .findAny()
+                .orElse(null);
+    }
+}
