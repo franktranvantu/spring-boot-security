b2476cd7ea3ff3dc56e81437bb178fa6794263c0
frank <franktranvantu@gmail.com>
2021-04-12 20:05:37 +0700

    Update

--- src/main/java/com/franktran/springbootsecurity/student/StudentController.java
+++ src/main/java/com/franktran/springbootsecurity/student/StudentController.java
@@ -1,6 +1,5 @@
-package com.franktran.springbootsecurity.controller;
+package com.franktran.springbootsecurity.student;
 
-import com.franktran.springbootsecurity.model.Student;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
