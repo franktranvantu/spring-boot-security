698555f9caaa376442fa7d16a31c252e6864c3c6
frank <franktranvantu@gmail.com>
2021-04-15 23:16:38 +0700

    Update

--- src/main/java/com/franktran/springbootsecurity/SpringBootSecurityApplication.java
+++ src/main/java/com/franktran/springbootsecurity/SpringBootSecurityApplication.java
@@ -2,8 +2,10 @@ package com.franktran.springbootsecurity;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 
 @SpringBootApplication
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SpringBootSecurityApplication {
 
     public static void main(String[] args) {
