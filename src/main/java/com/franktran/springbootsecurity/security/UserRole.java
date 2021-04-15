288cf60d25963391b90dedb50215a00ec05accf1
frank <franktranvantu@gmail.com>
2021-04-15 22:57:08 +0700

    Update

--- src/main/java/com/franktran/springbootsecurity/security/UserRole.java
+++ src/main/java/com/franktran/springbootsecurity/security/UserRole.java
@@ -7,27 +7,25 @@ import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import java.util.Set;
 import java.util.stream.Collectors;
 
-import static com.franktran.springbootsecurity.security.UserPermission.*;
-
 public enum UserRole {
 
-    ADMIN(Sets.newHashSet(STUDENT_READ, STUDENT_WRITE, COURSE_READ, COURSE_WRITE)),
-    ADMINTRAINEE(Sets.newHashSet(STUDENT_READ, COURSE_READ)),
+    ADMIN(Sets.newHashSet("STUDENT:READ", "STUDENT:WRITE")),
+    ADMINTRAINEE(Sets.newHashSet("STUDENT:READ")),
     STUDENT(Sets.newHashSet());
 
-    private final Set<UserPermission> permissions;
+    private final Set<String> permissions;
 
-    UserRole(Set<UserPermission> permissions) {
+    UserRole(Set<String> permissions) {
         this.permissions = permissions;
     }
 
-    public Set<UserPermission> getPermissions() {
+    public Set<String> getPermissions() {
         return permissions;
     }
 
     public Set<GrantedAuthority> getAuthorities() {
         Set<GrantedAuthority> authorities = permissions.stream()
-                .map(permission -> new SimpleGrantedAuthority(permission.getPermission()))
+                .map(permission -> new SimpleGrantedAuthority(permission))
                 .collect(Collectors.toSet());
         authorities.add(new SimpleGrantedAuthority("ROLE_" + name()));
         return authorities;
