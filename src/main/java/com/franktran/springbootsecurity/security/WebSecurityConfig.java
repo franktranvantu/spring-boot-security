f3b06f4b1c68739d7950f8f3776c6cc62d4928bc
franktranvantu <59758589+franktranvantu@users.noreply.github.com>
2021-04-16 12:53:03 +0700

    Update

--- src/main/java/com/franktran/springbootsecurity/security/WebSecurityConfig.java
+++ src/main/java/com/franktran/springbootsecurity/security/WebSecurityConfig.java
@@ -27,13 +27,13 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-                .csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse()).and()
+                .csrf().disable()
                 .authorizeRequests()
                 .antMatchers("/students/public").permitAll()
                 .anyRequest()
                 .authenticated()
                 .and()
-                .httpBasic();
+                .formLogin();
     }
 
     @Override
