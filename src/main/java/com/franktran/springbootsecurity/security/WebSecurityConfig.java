aec86608abc68e98ec7c9e8f5c84f4cbcc76814a
frank <franktranvantu@gmail.com>
2021-04-16 23:07:02 +0700

    Update

--- src/main/java/com/franktran/springbootsecurity/security/WebSecurityConfig.java
+++ src/main/java/com/franktran/springbootsecurity/security/WebSecurityConfig.java
@@ -1,11 +1,13 @@
 package com.franktran.springbootsecurity.security;
 
+import com.franktran.springbootsecurity.jwt.JwtUsernamePasswordAuthenticationFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
@@ -27,27 +29,12 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .csrf().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .addFilter(new JwtUsernamePasswordAuthenticationFilter(authenticationManager()))
                 .authorizeRequests()
                 .anyRequest()
-                .authenticated()
-                .and()
-                .formLogin()
-                    .loginPage("/show-login").permitAll()
-                    .loginProcessingUrl("/process-login")
-                    .defaultSuccessUrl("/management/students")
-                    .usernameParameter("email")
-                    .passwordParameter("password")
-                .and()
-                .rememberMe()
-                    .tokenValiditySeconds((int) TimeUnit.DAYS.toSeconds(20)).key("frank")
-                    .rememberMeParameter("remember")
-                .and().logout()
-                    .logoutUrl("/logout")
-                    .clearAuthentication(true)
-                    .invalidateHttpSession(true)
-                    .deleteCookies("JSESSIONID", "remember-me")
-                    .logoutSuccessUrl("/show-login");
-
+                .authenticated();
     }
 
     @Bean
