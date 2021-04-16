9a5d4b0772a4d389f2020c07e7cbd5dd9ec92c34
franktranvantu <59758589+franktranvantu@users.noreply.github.com>
2021-04-16 15:27:51 +0700

    Update

--- src/main/java/com/franktran/springbootsecurity/login/LoginController.java
+++ src/main/java/com/franktran/springbootsecurity/login/LoginController.java
@@ -2,21 +2,21 @@ package com.franktran.springbootsecurity.login;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class LoginController {
 
     @GetMapping("/show-login")
-    public String showLogin(@ModelAttribute("account") Account account) {
+    public String showLogin() {
         return "login";
     }
 
     @PostMapping("/process-login")
     @ResponseBody
-    public Account processLogin(Account account) {
-        return account;
+    public String processLogin(@RequestParam String username, @RequestParam String password) {
+        return String.format("Username: %s, Password: %s", username, password);
     }
 }
