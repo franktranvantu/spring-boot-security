05cbb432f9c3e08a8f0bbe5ae6a9ccfcb487607b
frank <franktranvantu@gmail.com>
2021-05-01 15:23:17 +0700

    Update

--- src/main/java/com/franktran/springbootsecurity/security/WebSecurityConfig.java
+++ src/main/java/com/franktran/springbootsecurity/security/WebSecurityConfig.java
@@ -1,7 +1,6 @@
 package com.franktran.springbootsecurity.security;
 
 import org.springframework.context.annotation.Bean;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -11,7 +10,6 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 import java.util.concurrent.TimeUnit;
 
